d99e8def3e76156260e1e29124f615325d56cbfc
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-07-24 03:45:54 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -6,7 +6,9 @@ def selects_all_female_bears_return_name_and_age
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+  "SELECT name
+  FROM bears
+  ORDER BY name"
 
 
 end
