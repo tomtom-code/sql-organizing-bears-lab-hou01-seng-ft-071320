3d14091ed53e99546378c271f57773f8af8f8257
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-07-24 04:14:02 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,7 +35,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT COUNT(color)
+  "SELECT color, COUNT(color)
   FROM bears
   WHERE color = 'dark brown';"
 end
